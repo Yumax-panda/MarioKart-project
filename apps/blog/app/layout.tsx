1c610edf4ce394dd81b101a8d4834c29e620e3fc
Yumax-panda <yuma0wall@gmail.com>
2025-09-21 17:01:31 +0900

    biomeを導入 & 不要な部分を除外

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -1,9 +1,6 @@
 import "@repo/ui/styles.css";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
-
-const geist = Geist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Turborepo",
@@ -16,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={geist.className}>{children}</body>
+    <html lang="ja">
+      <body>{children}</body>
     </html>
   );
 }
