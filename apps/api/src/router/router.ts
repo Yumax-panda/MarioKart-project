e3d4bb24f6fd353c861e7b0c082f674073a97c84
Yumax-panda <yuma0wall@gmail.com>
2025-10-08 02:57:12 +0900

    repository作成

--- apps/api/src/router/router.ts
+++ apps/api/src/router/router.ts
@@ -1,12 +1,10 @@
+import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
-import { createApp } from "./utils/factory";
 
-const app = createApp()
-  .use("*", prettyJSON(), logger())
-  .get("/", (c) => {
-    return c.text("Hello Hono!");
-  });
+const app = new Hono().use("*", prettyJSON(), logger()).get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
 export type AppType = typeof app;
 
