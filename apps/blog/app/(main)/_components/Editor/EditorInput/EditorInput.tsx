d097fcbef03c812b03505091b19f22bb79f6e85e
Yumax-panda <yuma0wall@gmail.com>
2025-10-13 05:15:23 +0900

    エディタ導入

---
+++ apps/blog/app/(main)/_components/Editor/EditorInput/EditorInput.tsx
@@ -0,0 +1,67 @@
+import { createStarryNight } from "@wooorm/starry-night";
+import sourceCss from "@wooorm/starry-night/source.css";
+import sourceJs from "@wooorm/starry-night/source.js";
+import sourceTs from "@wooorm/starry-night/source.ts";
+import sourceTsx from "@wooorm/starry-night/source.tsx";
+import textHtmlBasic from "@wooorm/starry-night/text.html.basic";
+import textMd from "@wooorm/starry-night/text.md";
+import { toJsxRuntime } from "hast-util-to-jsx-runtime";
+import { useEffect, useState } from "react";
+import { Fragment, jsx, jsxs } from "react/jsx-runtime";
+
+const grammars = [
+  sourceCss,
+  sourceJs,
+  sourceTsx,
+  sourceTs,
+  textHtmlBasic,
+  textMd,
+];
+
+type StarryNight = Awaited<ReturnType<typeof createStarryNight>>;
+
+type Props = {
+  currentInputValue: string;
+  onChange: (text: string) => void;
+};
+
+// TODO: CSS
+export const EditorInput = ({ currentInputValue, onChange }: Props) => {
+  const [starryNight, setStarryNight] = useState<StarryNight | null>(null);
+
+  useEffect(() => {
+    createStarryNight(grammars).then(setStarryNight);
+  }, []);
+
+  return (
+    <form className="editor">
+      <div className="editor-inner">
+        <div className="draw">
+          {starryNight !== null
+            ? toJsxRuntime(
+                starryNight.highlight(currentInputValue, "text.md"),
+                {
+                  Fragment,
+                  jsx,
+                  jsxs,
+                },
+              )
+            : undefined}
+          {/* Trailing whitespace in a `textarea` is shown, but not in a `div`
+          with `white-space: pre-wrap`.
+          Add a `br` to make the last newline explicit. */}
+          {/\n[ \t]*$/.test(currentInputValue) ? <br /> : undefined}
+        </div>
+        <textarea
+          spellCheck="false"
+          className="write"
+          value={currentInputValue}
+          rows={currentInputValue.split("\n").length + 1}
+          onChange={(event) => {
+            onChange(event.target.value);
+          }}
+        />
+      </div>
+    </form>
+  );
+};
---
+++ apps/blog/app/(main)/_components/Editor/Preview/Preview.tsx
@@ -0,0 +1,21 @@
+import { MarkdownHooks } from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import rehypeSlug from "rehype-slug";
+import rehypeStarryNight from "rehype-starry-night";
+import remarkGfm from "remark-gfm";
+import remarkToc from "remark-toc";
+
+const rehypePlugins = [rehypeRaw, rehypeSlug, rehypeStarryNight];
+const remarkPlugins = [remarkGfm, remarkToc];
+
+type Props = {
+  markdown: string;
+};
+
+export const Preview = ({ markdown }: Props) => {
+  return (
+    <MarkdownHooks rehypePlugins={rehypePlugins} remarkPlugins={remarkPlugins}>
+      {markdown}
+    </MarkdownHooks>
+  );
+};
