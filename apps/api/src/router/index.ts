3e3d032d63cd8f385aff29bb38ed925c81b18332
Yumax-panda <yuma0wall@gmail.com>
2025-10-22 11:52:11 +0900

    fix

--- apps/api/src/router/index.ts
+++ apps/api/src/router/index.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import { env } from "cloudflare:workers";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
@@ -27,38 +28,39 @@ import { auth } from "./auth";
 import type { Env } from "./types";
 import { v1 } from "./v1";
 
+// Dependency injection
+// 先にインスタンスを作成しておいて、ミドルウェアが呼ばれたときにはrepoを入れるだけにする
+// TODO: env.ENV_NAMEによってrepoをmockされたものへ変更したい
+const prisma = getPrisma(env.DATABASE_URL);
+container.registerInstance("PrismaClient", prisma);
+container.registerSingleton<AccountRepository>(
+  "AccountRepository",
+  AccountRepositoryImpl,
+);
+container.registerSingleton<AuthStateRepository>(
+  "AuthStateRepository",
+  AuthStateRepositoryImpl,
+);
+container.registerSingleton<PostRepository>(
+  "PostRepository",
+  PostRepositoryImpl,
+);
+container.registerSingleton<SessionRepository>(
+  "SessionRepository",
+  SessionRepositoryImpl,
+);
+container.registerSingleton<TagRepository>("TagRepository", TagRepositoryImpl);
+container.registerSingleton<UserRepository>(
+  "UserRepository",
+  UserRepositoryImpl,
+);
+container.registerSingleton<Repository>("Repository", RepositoryImpl);
+const repo = container.resolve<Repository>("Repository");
+
 const app = new Hono<Env>()
   .basePath("/api")
   .use(prettyJSON(), logger())
   .use(async (c, next) => {
-    const prisma = getPrisma(c.env.DATABASE_URL);
-    container.registerInstance("PrismaClient", prisma);
-    container.registerSingleton<AccountRepository>(
-      "AccountRepository",
-      AccountRepositoryImpl,
-    );
-    container.registerSingleton<AuthStateRepository>(
-      "AuthStateRepository",
-      AuthStateRepositoryImpl,
-    );
-    container.registerSingleton<PostRepository>(
-      "PostRepository",
-      PostRepositoryImpl,
-    );
-    container.registerSingleton<SessionRepository>(
-      "SessionRepository",
-      SessionRepositoryImpl,
-    );
-    container.registerSingleton<TagRepository>(
-      "TagRepository",
-      TagRepositoryImpl,
-    );
-    container.registerSingleton<UserRepository>(
-      "UserRepository",
-      UserRepositoryImpl,
-    );
-    container.registerSingleton<Repository>("Repository", RepositoryImpl);
-    const repo = container.resolve<Repository>("Repository");
     c.set("repo", repo);
     await next();
   })
