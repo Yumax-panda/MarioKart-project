24ea96f8e94851d7b9cfd81276008e97eacfb370
Yumax-panda <yuma0wall@gmail.com>
2025-11-05 12:04:21 +0900

    投稿記事アップデートのスキーマ定義

--- packages/schema/src/post.ts
+++ packages/schema/src/post.ts
@@ -1,14 +1,15 @@
 import { z } from "zod";
 import { DatetimeLikeStringSchema } from "./utils";
 
-const ArticleSchema = z.string().max(50000);
-const TitleSchema = z.string().max(255);
-const ThumbnailSchema = z.url();
+const ArticleSchema = z.string().trim().max(50000);
+const TitleSchema = z.string().trim().max(255);
+const ThumbnailSchema = z.url().trim();
+const TagsSchema = z.array(z.string().trim().max(15)).max(5);
 
 const BasePostSchema = z.object({
   id: z.string(),
   userId: z.string(),
-  tags: z.array(z.string().max(50)),
+  tags: TagsSchema,
   createdAt: DatetimeLikeStringSchema,
   updatedAt: DatetimeLikeStringSchema,
 });
@@ -40,3 +41,22 @@ export const GetPublishedPostListQuerySchema = z.object({
   page: z.coerce.number().min(1),
   perPage: z.coerce.number().max(12),
 });
+
+const UpdatePublishedPostBodySchema = z.object({
+  article: ArticleSchema,
+  title: TitleSchema,
+  thumbnail: ThumbnailSchema,
+  published: z.literal(true),
+});
+
+const UpdateNotPublishedPostBodySchema = z.object({
+  article: ArticleSchema.optional(),
+  title: TitleSchema.optional(),
+  thumbnail: ThumbnailSchema.optional(),
+  published: z.literal(false),
+});
+
+export const UpdatePostBodySchema = z.discriminatedUnion("published", [
+  UpdatePublishedPostBodySchema,
+  UpdateNotPublishedPostBodySchema,
+]);
