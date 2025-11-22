553aa8947bada381b6dc157c902e9ff19ad02eff
Yumax-panda <yuma0wall@gmail.com>
2025-11-23 03:45:06 +0900

    memo 化させて再レンダリングを回避

--- apps/blog/app/(main)/posts/[postId]/_components/Thumbnail/Thumbnail.tsx
+++ apps/blog/app/(main)/posts/[postId]/_components/Thumbnail/Thumbnail.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
+import { memo } from "react";
 
 type Props = {
   title: string;
   src: string;
 };
 
-export const Thumbnail = ({ src, title }: Props) => (
-  <div className="not-prose relative h-80 w-full overflow-hidden rounded-2xl">
-    <Image
-      src={src}
-      alt={`Thumbnail: ${title}`}
-      className="object-cover"
-      fill
-      loading="eager"
-    />
-  </div>
-);
+export const Thumbnail = memo(({ src, title }: Props) => {
+  return (
+    <div className="not-prose relative h-80 w-full overflow-hidden rounded-2xl">
+      <Image
+        src={src}
+        alt={`Thumbnail: ${title}`}
+        className="object-cover"
+        fill
+        loading="eager"
+      />
+    </div>
+  );
+});
