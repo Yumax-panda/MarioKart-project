9a18bf906a06ce538003b8c756f40e2fd149e5b2
Yumax-panda <yuma0wall@gmail.com>
2025-11-23 05:17:59 +0900

    見た目を修正

--- apps/blog/app/(main)/posts/[postId]/_components/PostPreview/PostPreview.tsx
+++ apps/blog/app/(main)/posts/[postId]/_components/PostPreview/PostPreview.tsx
@@ -19,8 +19,8 @@ export const PostPreview = ({
   authorName,
   authorImage,
 }: Props) => (
-  <ContentWrapper className="mx-auto flex flex-col gap-4">
-    <div className="mb-4 flex items-center justify-center">
+  <ContentWrapper className="mx-auto flex flex-col gap-4 pt-16">
+    <div className="flex items-center justify-center">
       <h1 className="text-center leading-tight" id={title}>
         {title}
       </h1>
