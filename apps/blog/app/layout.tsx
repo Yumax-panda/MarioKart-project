096c2c65b82168467bbe962840fb1d7a2ca18832
Yumax-panda <yuma0wall@gmail.com>
2025-12-05 14:56:53 +0900

    fix

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Provider } from "context/Provider";
 import type { Metadata } from "next";
-import { ReactDOM } from "next/dist/server/route-modules/app-page/vendored/rsc/entrypoints";
 
 export const metadata: Metadata = {
   title: "東工大マリオカートサークル",
@@ -13,11 +12,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // ref: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#link-relpreload
-  ReactDOM.preload(
-    "https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&family=Rajdhani:wght@300;400;500;600;700&display=swap",
-    { as: "style" },
-  );
   return (
     <html lang="ja">
       <Provider>
